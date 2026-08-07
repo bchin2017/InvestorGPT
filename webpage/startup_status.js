2f9a83f38610c202eaf18c6285e72b80f303e701
Hoe, Boon Ching <boon.ching.hoe@intel.com>
2026-08-07 14:13:44 +0800

    update md

--- webpage/startup_status.js
+++ webpage/startup_status.js
@@ -2,5 +2,5 @@ window.STARTUP_STATUS = {
   ready: 1,
   step: "Ready",
   message: "Dashboard is ready. Redirecting now...",
-  updatedAt: "Fri 07/08/2026 13:46:22.21"
+  updatedAt: "Fri 07/08/2026 14:11:18.70"
 };
